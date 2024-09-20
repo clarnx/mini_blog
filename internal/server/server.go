a074617099bcd525c4aa10774a252fe656107f5c
clarnx <clarenceonline@outlook.com>
2024-09-20 22:37:56 +0000

    Initial commit

---
+++ internal/server/server.go
@@ -0,0 +1,26 @@
+package server
+
+import (
+	"github.com/gofiber/fiber/v2"
+
+	"mini_blog/internal/database"
+)
+
+type FiberServer struct {
+	*fiber.App
+
+	db database.Service
+}
+
+func New() *FiberServer {
+	server := &FiberServer{
+		App: fiber.New(fiber.Config{
+			ServerHeader: "mini_blog",
+			AppName:      "mini_blog",
+		}),
+
+		db: database.New(),
+	}
+
+	return server
+}
